20c9a99de3adbef03ac82540249d3852901bc094
Genevieve Sampson <gsampson10@gmail.com>
2021-04-26 17:37:02 -0700

    update quotes

---
+++ quotes/quote_array.js
@@ -0,0 +1,11 @@
+var quoteArray = []
+quoteArray[0] = "Be a Girl with a mind, a Woman with attitude, and a Lady with class."
+quoteArray[1] = "She does not know what the future holds, but she is grateful for slow and steady growth."
+quoteArray[2] = "She believed she could, so she did."
+quoteArray[3] = "Your speed doesn't matter, forward is forward."
+quoteArray[4] = "Be a pineapple: stand tall, wear a crown, and be sweet on the inside."
+quoteArray[5] = "The world is your oyster."
+quoteArray[6] =  "You are braver than you believe, stronger than you seem and smarter than you think."
+
+
+
